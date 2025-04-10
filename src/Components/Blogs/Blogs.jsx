860b63e7b30fb88374ca0e500474313ffe928194
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-10 15:53:52 +0600

    added new aside table

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -11,9 +11,9 @@ const Blogs = ({ fetchPromise, handleFavorite, favorite }) => {
                     Discover and bid on extraordinary items
                 </p>
             </div>
-            <div className=''>
-                <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 lg:w-[70%]">
-                    <table className="table">
+            <div className='flex justify-between mt-12 gap-8 '>
+                <div className="py-3 overflow-x-auto rounded-3xl border border-base-content/5 bg-base-100 lg:w-[70%]">
+                    <table className=" table rounded-2xl border border-hidden border-[#443C68]">
                         {/* head */}
                         <thead>
                             <tr>
@@ -25,10 +25,53 @@ const Blogs = ({ fetchPromise, handleFavorite, favorite }) => {
                         </thead>
                         <tbody>
                             {
-                                data.map(item => <Blog favorite={favorite} item={item} key={item.id} handleFavorite={handleFavorite}/>)
+                                data.map(item => <Blog favorite={favorite} item={item} key={item.id} handleFavorite={handleFavorite} />)
                             }
                         </tbody>
                     </table>
+                </div>
+                <div className='w-[30%] p-5 bg-white  rounded-3xl'>
+                    {favorite.length === 0 ? (
+                        <div className='text-center  font-poppins py-14 '>
+                            <h1 className='text-xl font-semibold '>No favorites yet</h1>
+                            <p className='text-sm text-gray-400 font-light'>
+                                Click the heart icon on any item <br /> to add it to your favorites
+                            </p>
+                        </div>
+                    ) : (
+                        favorite.map(item =>
+                        (
+                            <div className="overflow-x-auto " key={item.id}>
+                                <table className=' table'>
+                                    <tbody className=''>
+                                        <tr className='rounded-box  border border-base-content/5 bg-base-100 my-2'>
+                                            <td>
+                                                <img src={item.image} alt='' className='w-12 h-12' />
+                                            </td>
+                                            <td>
+                                                <p>
+                                                    {item.title}
+                                                </p>
+                                                <div className='flex justify-around mt-2'>
+                                                    <p>
+                                                        ${item.currentBidPrice}
+                                                    </p>
+                                                    <p>Bids: {item.bidsCount}</p>
+                                                </div>
+                                            </td>
+                                            <td className=''>
+                                                <button className=' cursor-pointer text-xl '>
+                                                    x
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        )
+                        )
+                    )
+                    }
                 </div>
             </div>
         </div>
